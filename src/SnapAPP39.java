216708ad273136e3eb9a98bf27d8802820323b0d
grz28 <ghiffari28@gmail.com>
2018-10-29 11:28:45 +0700

    upload pertama boiii

---
+++ src/SnapAPP39.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Zaka
+ */
+public class SnapAPP39 extends SnapFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        SnapAPP39 a = new SnapAPP39();
+        a.setVisible(true);
+    }
+    
+}
